6a980865509f42ac54259be01423ffb481666389
Taj <tajmarie92@gmail.com>
2022-08-23 20:31:08 -0500

    Add project files.

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+
+  
+     public class BankAccount
+    {
+        private double balance=0;
+
+        public BankAccount()
+        {
+
+        }
+
+        public void Deposit(double depositAmount)
+        {
+            balance += depositAmount;
+            Console.WriteLine($"Simulating a deposit amount of {depositAmount, 0:c} to your account.");
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+       
+    }
+
+    
+    
+    
+}
